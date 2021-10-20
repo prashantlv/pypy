9e70b05608da593120648e31cabab31beed46d9c
outlier07 <72409748+outlier07@users.noreply.github.com>
2021-10-20 12:28:11 +0530

    Update weatherApi.py

--- weatherApi.py
+++ weatherApi.py
@@ -9,7 +9,7 @@ url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid={}'.format
 res = requests.get(url)     # request data from url thr request obj
 data = res.json()           # store json data in py dictonary
 
-# pprint(data)
+# pprint(data) for printing in human readable formate json
 name = data['name']
 humidity = data['main']['humidity']
 pressure = data['main']['pressure']
@@ -23,6 +23,4 @@ print('Air Pressure is :',pressure)
 print('Temperature is :',temp)
 print('Wind Speed is  :',windSpeed)
 print('Description :',desc)
-
-
 # api from https://home.openweathermap.org/
