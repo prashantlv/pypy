fa98c36a19a7dee51bb01990a123d32ef0ac05ec
Prashant Shukla <shukla.prashant689@gmail.com>
2022-10-08 15:15:40 +0530

    Update weatherApi.py

--- weatherApi.py
+++ weatherApi.py
@@ -1,13 +1,12 @@
 import os
 import requests
-from pprint import pprint
 
 city = input("Enter city :")
 APP_ID = os.environ.get("APPID")
 url = 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(city,APP_ID)
 
-res = requests.get(url)     # request data from url thr request obj
-data = res.json()           # store json data in py dictonary
+res = requests.get(url)
+data = res.json() 
 
 # pprint(data) for printing in human readable formate json
 name = data['name']
@@ -23,4 +22,5 @@ print('Air Pressure is :',pressure)
 print('Temperature is :',temp)
 print('Wind Speed is  :',windSpeed)
 print('Description :',desc)
-# api from https://home.openweathermap.org/
+
+#not complete
